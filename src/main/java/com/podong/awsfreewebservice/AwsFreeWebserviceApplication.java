9ec7b9a7958ce078c4f83777ba41a9ad5a6f1ef5
sgh15487 <sgh01101@naver.com>
2021-07-19 09:54:06 +0900

    Initial commit

---
+++ src/main/java/com/podong/awsfreewebservice/AwsFreeWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.podong.awsfreewebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsFreeWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsFreeWebserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/podong/awsfreewebservice/AwsFreeWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.podong.awsfreewebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsFreeWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
